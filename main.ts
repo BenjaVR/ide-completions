5980a7b11b350c0ffbee34d5bb9ab309e6f93d82
Benjamin Van Renterghem <benjamin@bvrcode.com>
2024-03-22 15:47:13 +0100

    Add another library that does work

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 export async function action() {
-  // Type here `return named` and it should complete and import `namedAction` from `remix-utils/named-action`.
+  // Type `axi` and it should complete and import `axios` from `axios`.
+  // axi
+
+  // Type `return named` and it should complete and import `namedAction` from `remix-utils/named-action`.
   // return named
 }
